8162128c1e36f65fdeaca8a38ae49c6a78e5ad3f
Stephan Butler <stephan@truevolve.technology>
2023-11-30 07:31:15 +0200

    better folder handling

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
 wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz 
 zcat < install-tl-unx.tar.gz | tar xf -
-cd install-tl-`date +%Y%m%d`
+
+# Get the list of directories matching the pattern "install-tl-*"
+# and store the first one in a variable
+folder=$(ls -d install-tl-*/ 2>/dev/null | head -n 1)
+
+# Check if the folder variable is empty
+if [ -z "$folder" ]; then
+    echo "Error: No folder found matching the pattern 'install-tl-*'"
+    exit 1
+fi
+
+# Change directory into the first folder
+cd "$folder" && echo "Changed directory to $folder"
+# Perform the install
 perl ./install-tl --no-interaction 
